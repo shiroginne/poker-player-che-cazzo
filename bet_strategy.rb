13cb1e2d90353c3e7e5346277685eb2798f10a8a
Evgeny Sugakov <shiroginne@users.noreply.github.com>
2022-08-24 14:20:25 +0200

    GO CRAZY

--- bet_strategy.rb
+++ bet_strategy.rb
@@ -10,8 +10,11 @@ class BetStrategy
 
   def call
     our_hand = game_state.our_hand
+    community = game_state.community_cards
     if our_hand.all?(&:high_value?) || game_state.our_hand.map(&:rank).uniq.count == 1 || our_hand[0].suit == our_hand[1].suit
       raise_bet
+    elsif our_hand.all?(&:high_value?) || community.any?(:high_value?)
+      double_raise_bet
     elsif our_hand.any?(&:high_value?)
       check
     else
@@ -31,5 +34,9 @@ class BetStrategy
 
   def raise_bet
     check + 50
+  end
+
+  def double_raise_bet
+    riase_bet * 2
   end
 end
