279c9fdd0c67ba3776fb8c1b4610dbef3163009f
Evgeny Sugakov <shiroginne@users.noreply.github.com>
2022-08-24 11:17:50 +0200

    Awesome

--- player.rb
+++ player.rb
@@ -1,12 +1,11 @@
 class Player
 
-  VERSION = "Default Ruby awesome player"
+  VERSION = "～(^з^)-☆"
 
   def bet_request(game_state)
     [0, 10].sample
   end
 
   def showdown(game_state)
-
   end
 end
