4cc39b8fac009412ad03be325012f2386243a425
Evgeny Sugakov <shiroginne@users.noreply.github.com>
2022-08-24 14:40:40 +0200

    fix

--- bet_strategy.rb
+++ bet_strategy.rb
@@ -12,11 +12,11 @@ class BetStrategy
   def call
     our_hand = game_state.our_hand
     community = game_state.community_cards
-    
+
     ranking = RainMan.new(game_state.all_cards).ranking
     puts "This is ranking ==> #{ranking}"
-    
-    if game_state.all_cards.map(&:rank).uniq.count == 1 || game_state.all_cards.map(&:suit).uniq.count == 1 || contain_pairs?
+
+    if game_state.all_cards.map(&:rank).uniq.count == 1 || game_state.all_cards.map(&:suit).uniq.count == 1
       double_raise_bet
     elsif our_hand.all?(&:high_value?)
       raise_bet
