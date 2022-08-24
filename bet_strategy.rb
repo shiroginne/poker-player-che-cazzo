23bb616b0ae26c539e9e7dc910bbe2fc8c079385
Martina Mazzanti <marti.mazzanti@live.it>
2022-08-24 14:24:01 +0200

    check all cards

--- bet_strategy.rb
+++ bet_strategy.rb
@@ -11,7 +11,7 @@ class BetStrategy
   def call
     our_hand = game_state.our_hand
     community = game_state.community_cards
-    if our_hand.all?(&:high_value?) || game_state.our_hand.map(&:rank).uniq.count == 1 || our_hand[0].suit == our_hand[1].suit
+    if our_hand.all?(&:high_value?) || game_state.all_cards.map(&:rank).uniq.count == 1 || game_state.all_cards.map(&:suit).uniq.count == 1
       double_raise_bet
     elsif our_hand.all?(&:high_value?) || community.any?(:high_value?)
       raise_bet
