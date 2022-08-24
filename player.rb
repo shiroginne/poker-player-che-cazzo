c7e3e6f05a97531405a039d893c01c8839f4b24a
Evgeny Sugakov <shiroginne@users.noreply.github.com>
2022-08-24 15:49:38 +0200

    Naming change

--- player.rb
+++ player.rb
@@ -2,7 +2,7 @@ require_relative "game_state"
 require_relative "bet_strategy"
 
 class Player
-  VERSION = "	^(#｀∀´)_Ψ We are coming for you SunflowerLXIX"
+  VERSION = "	^(#｀∀´)_Ψ Mess with the best - die like the rest"
 
   def bet_request(game_state)
     @game_state = GameState.new(game_state)
